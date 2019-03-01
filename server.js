8be39111addc17df8b33d4682e09efb26956f6bc
Adam Silver <adambsilver@gmail.com>
2019-03-01 13:03:24 +0000

    Fix up form validator so that gov.uk code is initialised

--- server.js
+++ server.js
@@ -61,6 +61,7 @@ app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, '/public')));
 app.use('/assets', express.static(path.join(__dirname, 'node_modules', 'govuk-frontend', 'assets')));
 app.use('/assets', express.static(path.join(__dirname, '/node_modules/@hmcts/frontend/assets')));
+app.use('/node_modules/govuk-frontend', express.static(path.join(__dirname, '/node_modules/govuk-frontend')));
 app.use('/node_modules/hmcts-frontend', express.static(path.join(__dirname, '/node_modules/@hmcts/frontend')));
 
 
